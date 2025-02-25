6a128412eefcba826773ee48fc9493ee9db37095
Jim Klo <jim.klo@sri.com>
2025-02-24 16:35:05 -0800

    cleaning up data

---
+++ public/leaderboard/assets/scripts/data.blank.js
@@ -0,0 +1,26 @@
+data = {
+    "challenges" : [
+        /*
+		{
+            "id": 1,
+            "name": "Challenge Name",
+            "allowed_time": "1:00:00.000000"
+        }, ...
+		*/
+    ],
+    // participants[x].challenges should only contain data for challenges that participant actually completed.
+    "participants": [
+        /*
+		{
+            "id": "P10001",
+            "handle": "username",
+            "challenges": [
+                {
+                    "id": 1,
+                    "time": "0:59:59.999999"
+                }, ...
+            ]
+        }, ...
+		*/
+    ]
+}
